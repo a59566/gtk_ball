ab37c9c667958de648b2233a61eea15545e42451
a59566 <a59566@outlook.com>
2015-12-27 22:29:30 +0800

    test_ver5
    add start page
    increase difficulty

--- gtkballt.c
+++ gtkballt.c
@@ -2,9 +2,9 @@
 #include <gtk/gtk.h>
 #include <math.h>
 #include <time.h>
-#define max_balls 3
+#define max_balls 100
 
-int ballcount = 1;
+int ballcount = 5;
 
 int i;
 int ball_x[max_balls] = {0};           //球圓心初始x座標
@@ -14,9 +14,9 @@ int x_vec[max_balls] = {0};            //球移動x向量
 int y_vec[max_balls] = {0};            //球移動y向量
 
 int board_x = 0;           //板子左上x座標
-int board_y = 280;         //板子左上y座標
-int board_width = 20;      //板子寬度
-int board_height = 20;     //板子厚度
+int board_y = 0;         //板子左上y座標
+int board_width = 0;      //板子寬度
+int board_height = 0;     //板子厚度
 
 int windows_height = 300;   //視窗高度
 int windows_weight = 400;   //視窗寬度
@@ -27,7 +27,10 @@ gint timeout_id;
 int game_over = TRUE;      //是否結束
 int pause = FALSE;
 
-
+GtkWidget *title;
+GtkWidget *start;
+GtkWidget *quit;
+GtkWidget *table;
 //繪製球
 void draw_ball(cairo_t *cr)
 {
@@ -62,16 +65,16 @@ void move_ball()
     for(i=0;i<ballcount;i++)
     {
         //到左邊界
-        if( ball_x[i] - r[i] <= 0 )        
+        if( ball_x[i] - r[i] <= 0 )
             x_vec[i] = -x_vec[i]+(rand()%5-2);
-        //到右邊界    
-        if( ball_x[i] + r[i] >= windows_weight )   
+        //到右邊界
+        if( ball_x[i] + r[i] >= windows_weight )
             x_vec[i] = -x_vec[i]+(rand()%5-2);
-        //到上邊界    
-        if( ball_y[i] - r[i] <= 20)        
+        //到上邊界
+        if( ball_y[i] - r[i] <= 20)
             y_vec[i] = -y_vec[i]+(rand()%5-2);
-        //到下邊界    
-        if( ball_y[i] + r[i] >= windows_height)        
+        //到下邊界
+        if( ball_y[i] + r[i] >= windows_height)
             y_vec[i] = -y_vec[i]+(rand()%5-2);
 
         //板子上
@@ -122,13 +125,13 @@ void on_key_press(GtkWindow *window, GdkEventKey *eventkey, gpointer data)
             else
                 board_x = windows_weight - board_width;
             break;
-        case 65362: //方向鍵 ↑    
+        case 65362: //方向鍵 ↑
             if(speed > 20)
             {
                 g_source_remove(timeout_id);
                 timeout_id = g_timeout_add(speed-=20, (GSourceFunc) time_handler, (gpointer) window);
-                board_move += 10;  
-            }   
+                board_move += 10;
+            }
             break;
         case 65364: //方向鍵 ↓
             g_source_remove(timeout_id);
@@ -153,13 +156,13 @@ void on_key_press(GtkWindow *window, GdkEventKey *eventkey, gpointer data)
             break;
         case 112:  //'p' 暫停控制
             if(pause)   //暫停取消
-            {         
+            {
                 g_source_remove(timeout_id);
                 timeout_id = g_timeout_add(speed, (GSourceFunc) time_handler, (gpointer) window);
                 pause = FALSE;
             }
             else    //暫停
-            {              
+            {
                 g_source_remove(timeout_id);
                 pause = TRUE;
             }
@@ -171,6 +174,11 @@ void on_key_press(GtkWindow *window, GdkEventKey *eventkey, gpointer data)
 //開始，初始化遊戲
 void on_start(GtkMenuItem *item,gpointer p)
 {
+    g_print ("\n ************\n *  start!  *\n ************\n\n");
+    gtk_widget_hide (table);
+    gtk_widget_hide (start);
+    gtk_widget_hide (quit);
+    gtk_widget_hide (title);
     for(i=0;i<ballcount;i++)
     {
         ball_x[i] =20;           //球圓心初始x座標
@@ -185,9 +193,9 @@ void on_start(GtkMenuItem *item,gpointer p)
     board_y = 280;         //板子左上y座標
     board_width = 20;      //板子寬度
     board_height = 20;     //板子厚度
-    
+
     speed = 100;
-    
+
     game_over = FALSE;
 }
 
@@ -214,34 +222,27 @@ int main(int argc,char **argv)
     gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
     //設定視窗寬&高
     gtk_window_set_default_size(GTK_WINDOW(window), windows_weight, windows_height);
-    GtkWidget *gamemenu;
-    GtkWidget *menubar;
-    GtkWidget *game;
-    GtkWidget *start;
-    GtkWidget *quit;
-    GtkWidget *vbox;
-    //建立vbox，把他加入容器內
-    vbox = gtk_vbox_new(FALSE,0);
-    gtk_container_add(GTK_CONTAINER(window),vbox);
-    //建立主選單
-    gamemenu = gtk_menu_new();
-    //建立選單列
-    menubar = gtk_menu_bar_new();
-    //建立選單選項
-    game = gtk_menu_item_new_with_label("Game");
-    start = gtk_menu_item_new_with_label("Start");
-    quit = gtk_menu_item_new_with_label("Quit");
 
-    gtk_menu_item_set_submenu(GTK_MENU_ITEM(game),gamemenu);
-    gtk_menu_shell_append(GTK_MENU_SHELL(gamemenu),start);
-    gtk_menu_shell_append(GTK_MENU_SHELL(gamemenu),quit);
-    gtk_menu_shell_append(GTK_MENU_SHELL(menubar),game);
+     /* 作出標題畫面 */
+    table = gtk_table_new (5, 5, TRUE);
+    gtk_container_add (GTK_CONTAINER (window), table);
 
-    gtk_box_pack_start(GTK_BOX(vbox),menubar,FALSE,FALSE,3);
+    title = gtk_label_new("BALL GAME");
+    gtk_table_attach_defaults (GTK_TABLE (table), title, 2, 3, 0, 1);
+    gtk_widget_show (title);
+
+    start = gtk_button_new_with_label ("START");
+    g_signal_connect (G_OBJECT (start), "clicked",G_CALLBACK (on_start), (gpointer) "start_button");
+    gtk_table_attach_defaults (GTK_TABLE (table), start, 1, 4, 1, 2);
+    gtk_widget_show (start);
+
+    quit = gtk_button_new_with_label ("Quit");
+    g_signal_connect (G_OBJECT (quit), "clicked",G_CALLBACK (gtk_main_quit), NULL);
+    gtk_table_attach_defaults (GTK_TABLE (table), quit, 1, 4, 3, 4);
+    gtk_widget_show (quit);
+    /* 作出標題畫面 */
     //加入開始選項對應的處理函式
-    g_signal_connect(G_OBJECT(start), "activate", G_CALLBACK(on_start), NULL);
-    
-    g_signal_connect(G_OBJECT(quit), "activate", G_CALLBACK(gtk_main_quit), NULL);
+
     gtk_widget_set_app_paintable(window, TRUE);
     gtk_widget_show_all(window);
     //加入time handler, 100ms
